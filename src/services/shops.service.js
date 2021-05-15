180f2b1486d03cb1f68e3e913313d1fb341e5294
Fernando <fernando461@gmail.com>
2021-05-14 21:35:13 -0300

    renaming

--- src/services/shops.service.js
+++ src/services/shops.service.js
@@ -3,8 +3,8 @@ const _ = require('lodash')
 const NotFoundException = require("../exceptions/not-found.exception");
 const { BusinessSearchResponseDTO } = require("../dto/business-search-response.dto");
 
-module.exports.searchBusinessesWithReviews = async (search ) => {
-    const searchBusinessesResponse = await yelpFusionClient.searchBusinesses(search)
+module.exports.searchBusinessesWithReviews = async (query) => {
+    const searchBusinessesResponse = await yelpFusionClient.searchBusinesses(query)
     if(_.isEmpty(searchBusinessesResponse?.businesses)) throw new NotFoundException(`No businesses found!`)
     return retrieveBusinessReviews(searchBusinessesResponse.businesses)
 }
